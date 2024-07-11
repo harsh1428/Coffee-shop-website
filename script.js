de5cc42a4fe91006555c4ddda3d83e327d9d3f89
Harsh Koolwal <134396371+harsh1428@users.noreply.github.com>
2024-07-11 12:47:08 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+let search = document.querySelector('.search-box')
+let searchbtn = document.querySelector('#search-icon')
+
+searchbtn.addEventListener('click',function(){
+    search.classList.toggle('active') ;
+})
+
+
